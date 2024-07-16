84dd0c8a2904e2eb1a17d3b547c665887dc6453d
jerevoss <jerevoss@gmail.com>
2024-07-16 10:57:27 -0700

    Python imag 1.4 runs

--- python/test_app.py
+++ python/test_app.py
@@ -79,7 +79,8 @@ def exception_request():
 if __name__ == "__main__":
     # Test imports of attach dependencies to detect breaking conflicts.
     # test_import_attach_dependencies()
-    port = environ["PYTHON_TEST_APP_PORT"]
+    # port = environ["PYTHON_TEST_APP_PORT"]
+    port = "8081"
     print("Server running at port: %s" % port)
     app.run(port=port, host='0.0.0.0')
 
