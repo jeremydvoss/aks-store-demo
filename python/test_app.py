692ef3154c1602b4df719340090c07e2b5e0b45f
jerevoss <jerevoss@gmail.com>
2024-07-16 13:25:48 -0700

    1.7 detailed response

--- python/test_app.py
+++ python/test_app.py
@@ -1,12 +1,12 @@
+from azure.monitor.opentelemetry import configure_azure_monitor
+
+configure_azure_monitor()
+
 from os import environ
 from opentelemetry import trace
 from flask import Flask, request
 import logging
 import requests
-
-from azure.monitor.opentelemetry import configure_azure_monitor
-
-configure_azure_monitor()
 
 app = Flask(__name__)
 logger = logging.getLogger(__name__)
@@ -16,7 +16,7 @@ logger.setLevel(logging.INFO)
 def server_request():
     print(request.args.get("param"))
     logger.info("request page")
-    return "Flask App"
+    return "Flask App (Instrumented with distro before imports)"
 
 @app.route("/dependencies")
 def dependencies_request():
